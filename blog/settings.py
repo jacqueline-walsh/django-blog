13c00fb06a3112ee287f75c8bbaa780ad984dc88
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2019-10-04 13:05:46 +0100

    Update settings and requirements file

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-simple-blog-app']
+ALLOWED_HOSTS = ['django-simple-blog-app.heroku.com']
 
 
 # Application definition
